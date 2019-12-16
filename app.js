c9a0741a3765781ff9ae4845b259935b0fa5ab27
Anders Svensson <andess86@gmail.com>
2019-12-16 01:56:07 +0100

    parse data buffer / move routes

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === '/') {
-    res.write('<html>');
-    res.write('<head><title>Enter Message</title></head>');
-    res.write('<body><h1>Hello from Node.js Server!</h1></body>');
-    res.write('</html>');
-    res.end();
-  }
-});
+const routes = require('./routes');
+
+const server = http.createServer(routes);
 
 server.listen(3000);
