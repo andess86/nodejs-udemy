3ccf6f3d4412661aa797bef6b2d7ac29c0f0c6a1
Anders Svensson <andess86@gmail.com>
2020-02-05 16:04:50 +0100

    Now serving html, add 404-page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
@@ -8,7 +9,12 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
+// Uses '/' as per default - this is our 'catch-all'-route
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
 app.listen(3000);
