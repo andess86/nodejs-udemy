9ee9f5d183dfec62f3e1474ced7f8e29e9e3c9cd
Anders Svensson <andess86@gmail.com>
2020-02-06 22:33:39 +0100

    Convert project to use ejs

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const expressHbs = require('express-handlebars');
 const app = express();
 
-app.engine(
-  'hbs',
-  expressHbs({
-    layoutsDir: 'views/layouts/',
-    defaultLayout: 'main-layout',
-    extname: 'hbs'
-  })
-);
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 // views is default views-folder in express.
 app.set('views', 'views');
 
@@ -27,7 +18,7 @@ app.use(shopRoutes);
 // Uses '/' as per default - this is our 'catch-all'-route
 app.use((req, res, next) => {
   // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '' });
 });
 
 app.listen(3000);
