19fa5e7e48de8c11d0d82d07adb55717c8e69a41
Anders Svensson <andess86@gmail.com>
2020-02-06 21:56:37 +0100

    Convert project to use express-handlebars

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-
+const expressHbs = require('express-handlebars');
 const app = express();
 
-app.set('view engine', 'pug');
-
+app.engine(
+  'hbs',
+  expressHbs({
+    layoutsDir: 'views/layouts/',
+    defaultLayout: 'main-layout',
+    extname: 'hbs'
+  })
+);
+app.set('view engine', 'hbs');
 // views is default views-folder in express.
-app.set('views', 'views')
+app.set('views', 'views');
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
