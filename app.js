61dfa5d05d6bee26cacc205740cc7b79f41117cd
Anders Svensson <andess86@gmail.com>
2020-02-06 19:57:01 +0100

    Added pug layout file and extend from that

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(shopRoutes);
 // Uses '/' as per default - this is our 'catch-all'-route
 app.use((req, res, next) => {
   // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-  res.status(404).render('404');
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
