ab2f939898d1c07e021c8012e8564b02a0a95c76
Anders Svensson <andess86@gmail.com>
2020-02-07 20:07:11 +0100

    Added views, routes and controllers for admin, cart, checkout

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const path = require('path');
 
-const productsController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 const router = express.Router();
 
 
 // Reached under /admin/
-router.get('/add-product', productsController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
 
-router.post('/add-product', productsController.postAddProduct);
+router.get('/products', adminController.getProducts);
+
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
