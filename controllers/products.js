448b5ad91f60413f22f096fc3642a09ed8555faf
Anders Svensson <andess86@gmail.com>
2020-02-07 19:00:44 +0100

    Save on file

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,5 @@
 const Product = require('../models/product');
 exports.getAddProduct = (req, res, next) => {
-  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
   res.render('add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
@@ -11,20 +10,20 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const product = new Product (req.body.title);
+  const product = new Product(req.body.title);
   product.save();
   res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
-  const products = Product.fetchAll();
-  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
+  Product.fetchAll(products => {
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
   });
 };
